f68f91a61ed042c8ed6fc840342e44f0c66eb772
Oli Nelson <olivernelson@icloud.com>
2019-03-07 09:17:35 -0500

    change artist serializer

--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -1,5 +1,5 @@
 class ArtistSerializer < ActiveModel::Serializer
-  attributes :id, :name, :bio, :genres, :events
+  attributes :id, :name, :bio, :genres, :events, :image_url, :thumb_url, :facebook_page_url
   has_many :artist_genres
   has_many :genres, through: :artist_genres
   has_many :events
